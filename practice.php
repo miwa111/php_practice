97712526de005c246735d0959e35fd93b56d6933
miwako <mtxeusaiopc@gmail.com>
2021-10-26 05:19:16 +0000

    php

--- practice.php
+++ practice.php
@@ -6,6 +6,7 @@ echo "「私は　あなたの名前　です」";
 } else {
 echo "「私はあなたの名前ではありません」";
 }
+echo "\n";
 
 $total = 0;
 for ($i = 0; $i <= 10000; $i++) {
@@ -13,10 +14,12 @@ $total += $i;
 }
 echo $total;
 
+echo "\n";
+
 $fruits = array("イチゴ", "モモ", "ナシ", "ブドウ", "リンゴ",);
 foreach($fruits as $fruits){
 echo $fruits;
-echo "¥n";
+echo "\n";
 }
 
 /* for文の始めの値を定義する */
@@ -29,6 +32,6 @@ for($i = $start; $i < $end; $i++){
   // 5で割り切れたら{}内を実行する
   if($i % 5 == 0){
     echo $i;
-    echo "¥n";
+    echo "\n";
   }
 }
