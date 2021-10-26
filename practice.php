24ffb20579b69e3411245a4e255c17ce3d6c44de
miwako <mtxeusaiopc@gmail.com>
2021-10-26 04:33:35 +0000

    表示テキストを修正

--- practice.php
+++ practice.php
@@ -1,5 +1,18 @@
-<?php
-echo 'hello php!';
+$name = Miwako;
+if ($name = Miwako)  {
+echo "「私は　あなたの名前　です」";
+} else {
+echo "「私はあなたの名前ではありません」";
+}
 
-$a = 8;
-echo $a*3;
+$total = 0;
+for ($i = 0; $i <= 10000; $i++) {
+$total += $i;
+}
+echo $total;
+
+$fruits = array("straberry", "peach", "pear", "grape", "apple",);
+foreach($fruits as $fruits){
+echo "$fruits";
+echo "/n";
+}
